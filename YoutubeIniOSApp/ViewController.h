2576b791451cbe981e88b84eda5be110c4c2d0bc
surajwebo <surajmirajkar@rediffmail.com>
2015-06-03 11:28:01 +0530

    Initial Commit

---
+++ YoutubeIniOSApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YoutubeIniOSApp
+//
+//  Created by Suraj on 03/06/15.
+//  Copyright (c) 2015 Suraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YoutubeIniOSApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YoutubeIniOSApp
+//
+//  Created by Suraj on 03/06/15.
+//  Copyright (c) 2015 Suraj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
