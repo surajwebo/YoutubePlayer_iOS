03251e64b8c45223d1b5873d4e9ee007ddf6ec2b
surajwebo <surajmirajkar@rediffmail.com>
2015-06-03 15:45:42 +0530

    Embedding Youtube iOS Player Helper
    Playing Youtube video in iOS App.
    Full native control to Play, Pause, Stop & Seek to time video.

--- YoutubeIniOSApp/ViewController.h
+++ YoutubeIniOSApp/ViewController.h
@@ -7,9 +7,22 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "YTPlayerView.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <YTPlayerViewDelegate> {
+    
+}
 
+@property(nonatomic, strong) IBOutlet YTPlayerView *youtubePlayerView;
+@property(nonatomic, weak) IBOutlet UISlider *slider;
+@property(nonatomic, weak) IBOutlet UIButton *playButton;
+@property(nonatomic, weak) IBOutlet UIButton *pauseButton;
+@property(nonatomic, weak) IBOutlet UIButton *stopButton;
+@property(nonatomic, weak) IBOutlet UILabel  *lblStartTime;
+@property(nonatomic, weak) IBOutlet UILabel  *lblDuration;
+
+- (IBAction)onSliderChange:(id)sender;
+- (IBAction)buttonPressed:(id)sender;
 
 @end
 
--- YoutubeIniOSApp/ViewController.m
+++ YoutubeIniOSApp/ViewController.m
@@ -16,12 +16,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *videoId = @"Xk1zfQOklJY";
+    NSDictionary *playerVars = @{@"controls" : @0,
+                                 @"playsinline" : @1,
+                                 @"autohide" : @1,
+                                 @"showinfo" : @0,
+                                 @"modestbranding" : @1};
+    self.youtubePlayerView.delegate = self;
+    [self.youtubePlayerView loadWithVideoId:videoId playerVars:playerVars];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(receivedPlaybackStartedNotification:)
+                                                 name:@"Playback started"
+                                               object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)playerView:(YTPlayerView *)ytPlayerView didChangeToState:(YTPlayerState)state {
+    NSLog(@"%@",[NSString stringWithFormat:@"Player state changed: %ld\n", (long)state]);
+}
+
+- (void)playerView:(YTPlayerView *)playerView didPlayTime:(float)playTime {
+    float progress = playTime/self.youtubePlayerView.duration;
+    [self.slider setValue:progress];
+    [self.lblStartTime setText:[NSString stringWithFormat:@"%.2f",self.youtubePlayerView.currentTime/60]];
+    [self.lblDuration  setText:[NSString stringWithFormat:@"%.2f",self.youtubePlayerView.duration/60]];
+}
+
+- (IBAction)onSliderChange:(id)sender {
+    float seekToTime = self.youtubePlayerView.duration * self.slider.value;
+    [self.youtubePlayerView seekToSeconds:seekToTime allowSeekAhead:YES];
+    //[self appendStatusText:[NSString stringWithFormat:@"Seeking to time: %.0f seconds\n", seekToTime]];
+}
+
+- (IBAction)buttonPressed:(id)sender {
+    if (sender == self.playButton) {
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"Playback started" object:self];
+        [self.youtubePlayerView playVideo];
+    } else if (sender == self.stopButton) {
+        [self.youtubePlayerView stopVideo];
+        [self.slider setValue:0];
+        [self.lblStartTime setText:@"0.0"];
+        [self.lblDuration  setText:@"0.0"];
+    } else if (sender == self.pauseButton) {
+        [self.youtubePlayerView pauseVideo];
+    }
+}
+
+- (void)receivedPlaybackStartedNotification:(NSNotification *) notification {
+    if([notification.name isEqual:@"Playback started"] && notification.object != self) {
+        [self.youtubePlayerView pauseVideo];
+    }
+}
+
 
 @end
